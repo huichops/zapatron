aec2372aec2ea37e223ad296e6bdd5a442d4208e
Luis Argumedo <huicho.argumedo@gmail.com>
2013-11-02 17:33:03 -0600

    cambio a public_folder

--- controllers/AppController.rb
+++ controllers/AppController.rb
@@ -2,8 +2,9 @@ require 'sinatra/base'
 
 class AppController < Sinatra::Base
   enable :sessions, :method_override
+
   set :views, File.expand_path('../../views', __FILE__)
-  set :public, File.expand_path('../../public', __FILE__)
+  set :public_folder, File.expand_path('../../public', __FILE__)
 
   not_found{ p ".l." }
 
