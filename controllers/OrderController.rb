7cea4bfa2c2b55d764ba646c7a42c0d6e234ec1c
Almand <almand@localhost.lan>
2013-12-05 12:32:11 -0600

    GRUD controlador terminado, calalo Mae

--- controllers/OrderController.rb
+++ controllers/OrderController.rb
@@ -4,10 +4,30 @@ class OrderController <AppController
     get '/' do
         @orders = Order.all :order=> :id.desc
         @title = 'Pedido'
-        erb :orders
+        erb :OrdersIndex
     end
 
     post '/' do
+        o =Order.new
+        o.nombre = params[:id]
+        erb :OrderEdit
+    end
+
+    put '/:id'do
+        o = Order.get param[:id]
+        o.nombre = params[:nombre]
+        o.save
+        redirect '/orders'
+    end
+    get '/id/delete' do
+        @order = order.get params[id]
+        erb :OrderDelete
+    end
+
+    delete '/:id' do
+        o = order.get params[:id]
+        o.destroy
+        redirect '/orders'
     end
 end
 
