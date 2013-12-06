264251ea27485b5b67840d71deefc9c863eacfbd
Renato <renatocrobledo@gmail.com>
2013-12-06 15:36:18 -0600

    Shoe controller modificado

--- controllers/ShoeController.rb
+++ controllers/ShoeController.rb
@@ -9,8 +9,34 @@ class ShoeController < AppController
 
   post '/' do
     s = Shoe.new
-    c.model = params[:model]
-    c.save
+    s.model = params[:model]
+    s.save
 	redirect '/shoes'
+  end
+end
+
+get '/:id' do
+    @shoes = Shoe.get params[:id]
+    @title = 'Editar Modelo'
+    erb :ShoeEdit
+  end
+
+  put '/:id' do
+    s = Shoe.get params[:id]
+    s.nombre = params[:nombre]
+    s.save
+    redirect '/shoes'
+  end
+
+  get '/:id/delete' do
+    @shoes = Shoe.get params[:id]
+    @title = 'Eliminar modelo'
+    erb :ShoeDelete
+  end
+
+  delete '/:id' do
+    s = Color.get params[:id]
+    s.destroy
+    redirect '/shoes'
   end
 end
