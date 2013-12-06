386e1dd95eb2065c9d7e1ee43f5f925e585f7d24
Almand <almand@localhost.lan>
2013-12-06 09:05:38 -0600

    Agregada la fecha a Pedidos

--- models/Order.rb
+++ models/Order.rb
@@ -1,9 +1,11 @@
 require 'dm-core'
 require 'dm-migrations'
+require 'date'
 
 class Order
     include DataMapper::Resource
     property :id, Serial
     property :nombre, String
+    property :fecha, Date
     DataMapper.finalize
 end
